a45fdfb8fd489a2977da33d07f96f16d4db1eaa8
Rizwan Ali <rizwansoomro0317gmail.com>
2025-11-07 18:24:29 +0500

    feat: add initial project files

---
+++ app.js
@@ -0,0 +1,11 @@
+var isPositive = false;
+var number = 11;
+
+if (number  % 2 === 0) {
+    alert("Number is Even");
+} else {
+    alert("Number is Odd");
+}
+
+const students = ['Ali', 'Ahmed', 'Alisha', 'Ayesha', 'Amir'];
+console.table(students);
