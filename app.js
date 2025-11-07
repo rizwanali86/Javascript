7e9ed5abd555b26044ab05dfa477a855280f03f8
Rizwan Ali <rizwansoomro0317gmail.com>
2025-11-07 19:01:36 +0500

    Feat: add more data

--- app.js
+++ app.js
@@ -1,11 +1,40 @@
-var isPositive = false;
-var number = 11;
+// var isPositive = false;
+// var number = 11;
 
-if (number  % 2 === 0) {
-    alert("Number is Even");
-} else {
-    alert("Number is Odd");
+// if (number  % 2 === 0) {
+//     alert("Number is Even");
+// } else {
+//     alert("Number is Odd");
+// }
+
+// const students = ['Ali', 'Ahmed', 'Alisha', 'Ayesha', 'Amir'];
+// console.table(students);
+
+// let age = 17;
+// if (age >= 18) {
+//     console.log('Adult');
+// }else {
+//     console.log('Minor');
+// }
+
+// let marks = 85;
+// if (marks > 90) {
+//     console.log('A');
+// } else if (marks > 75) {
+//     console.log('B');
+// } else console.log('C');
+
+
+let marks = prompt("Enter your marks:");
+
+if  (marks < 0 || marks > 100) {
+    alert("Invalid marks");
+} else if (marks >= 90) {
+    alert("COngratulations! You got A* grade.");
+} else if (marks >= 80) {
+    alert("Hurray! You got A+ garde.");
+} else if (marks >= 70) {
+    alert("You got A grade.");
+} else if (marks < 70 && marks >= 60) {
+    alert("Failed")
 }
-
-const students = ['Ali', 'Ahmed', 'Alisha', 'Ayesha', 'Amir'];
-console.table(students);
